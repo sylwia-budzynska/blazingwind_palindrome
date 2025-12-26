e33d4394cfa4c804cc2be58d989453bb12288e7a
Sylwia Budzynska <102833689+sylwia-budzynska@users.noreply.github.com>
2025-12-26 22:26:06 +0000

    Initialize repo

---
+++ lib/blazingwind_palindrome.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "blazingwind_palindrome/version"
+
+module BlazingwindPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
